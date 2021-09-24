971a72eda974d6f2e2143e594ec3c3164c1d33a1
Md Moniruzzaman <33169928+Monirs@users.noreply.github.com>
2021-09-24 00:50:20 -0400

    Create AddTwoNumbers.py

---
+++ AddTwoNumbers.py
@@ -0,0 +1,12 @@
+# Store input numbers
+num1 = input('Enter first number: ')
+num2 = input('Enter second number: ')
+num3 = input('Enter third number: ')
+
+# Add two numbers
+sum = float(num1) + float(num2) + float(num3)
+
+# Display the sum
+print('The sum of {0} {1} and {2} is {3}'.format(num1, num2, num3, sum))
+
+
